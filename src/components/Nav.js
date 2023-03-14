ec46dc0e09d7b48afe9f285549a980f9f0f7e986
erinmd <erin817@gmail.com>
2023-03-14 14:37:05 +0000

    user avatar added to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,11 @@
+import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { UserContext } from '../contexts/User'
 import { SelectCategory } from './SelectCategory'
 
-export const Nav = ({
-  currentCategory,
-  setCurrentCategory,
-}) => {
+export const Nav = ({ currentCategory, setCurrentCategory }) => {
   const navigate = useNavigate()
-
+  const { user } = useContext(UserContext)
   const handleSubmit = e => {
     e.preventDefault()
     if (currentCategory !== 'Select Category') {
@@ -16,16 +15,23 @@ export const Nav = ({
       }
     }
   }
-  
+
   return (
     <nav>
-      <Link className='linkText' to='/'>
-        Home
-      </Link>
-      <form onSubmit={handleSubmit}>
-        <SelectCategory setCurrentCategory={setCurrentCategory} />
-        <button className='navButton'> Go </button>
-      </form>
+      <section className='navContainer'>
+        <Link className='linkText' to='/'>
+          Home
+        </Link>
+        <form className='catForm' onSubmit={handleSubmit}>
+          <SelectCategory setCurrentCategory={setCurrentCategory} />
+          <button className='navButton'> Go </button>
+        </form>
+      </section>
+      <img
+        className='navAvatar'
+        src={user.avatar_url}
+        alt={`Avatar for ${user.username}`}
+      />
     </nav>
   )
 }
