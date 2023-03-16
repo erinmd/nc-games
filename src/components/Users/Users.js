a0883c96f25683f76509d00e7d7d9d11e3c774ca
erinmd <erin817@gmail.com>
2023-03-16 09:27:38 +0000

    current user card added

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,3 +1,19 @@
+import { useContext } from "react"
+import { UserContext } from "../../contexts/User"
+
 export const Users = () => {
-    return <h2>Users</h2>
+    const {user} = useContext(UserContext)
+    return <section>
+        <h2>Users</h2>
+        <ol>
+            <li className='userCard'>
+                <div className='userProfileText'>
+                <h3>Current user</h3>
+                <p>Username: {user.username}</p>
+                <p>Name: {user.name}</p>
+                </div>
+                <img className='userProfileAvatar' src={user.avatar_url} alt={user.username}/>
+            </li>
+        </ol>
+        </section>
 }
