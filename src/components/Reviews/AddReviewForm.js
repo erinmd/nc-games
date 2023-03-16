d0df4b2c75395fc9462114a612b3079e9f989f78
erinmd <erin817@gmail.com>
2023-03-16 11:53:47 +0000

    initial form validation set up

--- src/components/Reviews/AddReviewForm.js
+++ src/components/Reviews/AddReviewForm.js
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import { SelectCategory } from '../Nav/SelectCategory'
 
-export const AddReviewForm = ({setNewReview}) => {
+export const AddReviewForm = ({ setNewReview }) => {
   const [formFields, setFormFields] = useState({
     title: '',
     body: '',
     designer: '',
     image: ''
   })
+  const [feedback, setFeedback] = useState({
+    title: { msg: '', class: 'form-none' },
+    body: { msg: '', class: 'form-none' },
+    designer: { msg: '', class: 'form-none' },
+    image: { msg: '', class: 'form-none' },
+    category: { msg: '', class: 'form-none' }
+  })
   const [category, setCategory] = useState(null)
 
   const submitHandler = e => {
     e.preventDefault()
-    setNewReview({...formFields, category})
+    setNewReview({ ...formFields, category })
   }
 
   return (
@@ -25,7 +32,23 @@ export const AddReviewForm = ({setNewReview}) => {
           }}
           value={formFields.title}
           id='reviewTitle'
+          onBlur={e => {
+            if (e.target.value.length === 0) {
+              setFeedback({
+                ...feedback,
+                title: { msg: 'Required', class: 'error' }
+              })
+            } else {
+                setFeedback({
+                    ...feedback,
+                    title: { msg: '', class: 'form-none' }
+                })
+            }
+          }}
         />
+        <label htmlFor='reviewTitle' className={feedback.title.class}>
+          {feedback.title.msg}
+        </label>
       </section>
       <section>
         <label htmlFor='reviewBody'>Review:</label>
@@ -34,7 +57,23 @@ export const AddReviewForm = ({setNewReview}) => {
           onChange={e => {
             setFormFields({ ...formFields, body: e.target.value })
           }}
+          onBlur={e => {
+            if (e.target.value.length === 0) {
+              setFeedback({
+                ...feedback,
+                body: { msg: 'Required', class: 'error' }
+              })
+            } else {
+                setFeedback({
+                    ...feedback,
+                    body: { msg: '', class: 'form-none' }
+                })
+            }
+          }}
         />
+        <label htmlFor='reviewBody' className={feedback.body.class}>
+          {feedback.body.msg}
+        </label>
       </section>
       <section>
         <label htmlFor='reviewDesigner'>Designer:</label>
@@ -44,11 +83,42 @@ export const AddReviewForm = ({setNewReview}) => {
           onChange={e => {
             setFormFields({ ...formFields, designer: e.target.value })
           }}
+          onBlur={e => {
+            if (e.target.value.length === 0) {
+              setFeedback({
+                ...feedback,
+                designer: { msg: 'Required', class: 'error' }
+              })
+            } else {
+                setFeedback({
+                    ...feedback,
+                    designer: { msg: '', class: 'form-none' }
+                })
+            }
+          }}
         />
+        <label htmlFor='reviewDesigner' className={feedback.designer.class}>
+          {feedback.designer.msg}
+        </label>
       </section>
       <section>
         <label htmlFor='categorySelect'>Category:</label>
-        <SelectCategory setCurrentCategory={setCategory} id='categorySelect' />
+        <SelectCategory setCurrentCategory={setCategory} id='categorySelect' onBlur={e => {
+            if (e.target.value=== 'Select Category') {
+              setFeedback({
+                ...feedback,
+                category: { msg: 'Required', class: 'error' }
+              })
+            } else {
+                setFeedback({
+                    ...feedback,
+                    category: { msg: 'anything', class: 'form-none' }
+                })
+            }
+          }}/>
+        <label htmlFor='reviewTitle' className={feedback.category.class}>
+          {feedback.category.msg}
+        </label>
       </section>
       <section>
         <label htmlFor='reviewImage'>Image URL:</label>
@@ -58,7 +128,23 @@ export const AddReviewForm = ({setNewReview}) => {
           onChange={e => {
             setFormFields({ ...formFields, image: e.target.value })
           }}
+          onBlur={e => {
+            if (e.target.value.length === 0) {
+              setFeedback({
+                ...feedback,
+                image: { msg: 'A default image will be selected for you', class: 'success' }
+              })
+            } else {
+                setFeedback({
+                    ...feedback,
+                    image: { msg: '', class: 'form-none' }
+                })
+            }
+          }}
         />
+        <label htmlFor='reviewImage' className={feedback.image.class}>
+          {feedback.image.msg}
+        </label>
       </section>
       <section>
         <button className='addReviewButton'>Submit</button>
